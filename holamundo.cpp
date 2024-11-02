6a7f536cc10ee21c3da8e6e918e304cb17f9dae3
juan manuel andino <andino_2269@hotmail.com>
2024-11-01 21:28:10 -0300

    ahi va

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Incluye la biblioteca de entrada y salida
 
 int main() {
-    std::cout << "Hola, mundo" << std::endl; // Imprime el mensaje en pantalla
+    std::cout << "Hola, mundo loco" << std::endl; // Imprime el mensaje en pantalla
     return 0; // Indica que el programa terminó correctamente
 }
